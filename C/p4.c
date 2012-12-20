7c3657796692c6d26748b9fd57ddaa89e0273f27
Frank Massi <frankmassi@gmail.com>
2012-12-19 22:41:55 -0600

    Prob. 4 - includes base^exp and int = palindrome? functions

--- C/p4.c
+++ C/p4.c
@@ -9,7 +9,7 @@
 *	Find the largest palindrome made 			*
 *   from the product of two 3-digit numbers		*
 *												*
-*	Includes a palindrome checker				*
+*	Includes a palindrome checker	 			*
 *	Includes a exponent program		 			*
 *												*
 ************************************************/
