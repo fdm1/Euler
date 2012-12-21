b1349acb08e4338dad7ef3f2a8d9b527b0474d86
Frank Massi <frankmassi@gmail.com>
2012-12-20 21:38:05 -0600

    numbers are too big...110k+ prime numbers, most over 1million. Need faster prime calculator, and way to add all the numbers

--- C/c10.c
+++ C/c10.c
@@ -11,32 +11,54 @@ bool isPrime(int number)
 {
 	int j = 2;
 
+	//if number is 2 -> true
+	if (j == number)
+		return true;
+
+	//if number is less than 2 -> false
 	if (j > number)
 		return false;
 
-	while(j < number)
+	//if number > 2 and even -> false
+	if ((number % 2 == 0) && (number > 2))
+		return false;
+
+	//if number is less than j, increment to 3, and then up by odd numbers. If divisible -> false
+	while (j <= number)
 	{
-		if (number % j == 0)
+
+		j++;
+		if ((number % j == 0) && (number > j))
 			return false;
+
+		if (number == j)
+			return true;
 
 		j++;
 	}
 
-	if (j == number)
-		return true;
 }
 
 int main (void)
 {
-	int sum = 0;
-	for (int i = 0; i < 2000000; i++)
+	long sum = 0;
+	int i = 1;
+	int count = 0;
+	while (i < 2000000)
 	{
+
+		i++;
 		if (isPrime(i))
 		{
-			sum += i;
+			//sum += i;
+			count++;
+			printf("%d\n", count);
+
+			if (i != 2)
+				i++;
 		}
+		
 	}	
 
-	printf("%d", sum);
-	return;
+	//printf("%lu", sum);
 }
