fe929aaab057af2b2812a8a82b331b1f3abb846b
Frank Massi <frankmassi@gmail.com>
2012-12-20 21:52:20 -0600

    trying to speed up prime calc. - include blocks for low prime numbers, still too slow

--- C/c10.c
+++ C/c10.c
@@ -20,9 +20,25 @@ bool isPrime(int number)
 		return false;
 
 	//if number > 2 and even -> false
-	if ((number % 2 == 0) && (number > 2))
-		return false;
+	if ((number % 2 == 0) && (number > 2)) return false;
 
+		//if number > 2 and even -> false
+	if ((number % 3 == 0) && (number > 3)) return false;
+
+	//if number > 2 and even -> false
+	if ((number % 5 == 0) && (number > 5)) return false;
+
+		//if number > 2 and even -> false
+	if ((number % 7 == 0) && (number > 7)) return false;
+	if ((number % 11 == 0) && (number > 11)) return false;
+	if ((number % 13 == 0) && (number > 13)) return false;
+	if ((number % 17 == 0) && (number > 17)) return false;
+	if ((number % 19 == 0) && (number > 19)) return false;
+	if ((number % 23 == 0) && (number > 23)) return false;
+	if ((number % 29 == 0) && (number > 29)) return false;
+	if ((number % 31 == 0) && (number > 31)) return false;
+	if ((number % 37 == 0) && (number > 37)) return false;
+	if ((number % 41 == 0) && (number > 41)) return false;
 	//if number is less than j, increment to 3, and then up by odd numbers. If divisible -> false
 	while (j <= number)
 	{
@@ -44,6 +60,11 @@ int main (void)
 	long sum = 0;
 	int i = 1;
 	int count = 0;
+
+	//generate 1st 100 prime numbers into an array
+	//pass the array into the prime checker
+	// 	if ((number % prime[i] == 0) && (number > prime[i])) return false;
+
 	while (i < 2000000)
 	{
 
@@ -52,13 +73,13 @@ int main (void)
 		{
 			//sum += i;
 			count++;
-			printf("%d\n", count);
+			
 
 			if (i != 2)
 				i++;
 		}
 		
 	}	
-
+printf("%d\n", count);
 	//printf("%lu", sum);
 }
