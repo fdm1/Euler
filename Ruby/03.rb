7667267ad82eebc71712678f9bc5a73b4277407f
Frank Massi <frankmassi@gmail.com>
2013-05-17 19:43:47 -0500

    03 Ruby

--- Ruby/03.rb
+++ Ruby/03.rb
@@ -3,6 +3,24 @@
 # *
 # *	Find the largest prime factor of the number 600851475143
 # *
-# *	Includes a prime number verifying sub-function
-# *
 # **************************************/
+
+class Euler3
+
+	TARGET = 600851475143
+	mod = TARGET
+
+	i = 2.0
+
+	while mod > i do
+
+		while mod % i == 0 do
+			mod = mod / i 
+		end
+
+		i = i + 1
+	end
+
+	puts i
+
+end
