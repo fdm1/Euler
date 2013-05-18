774100ac9fb796a2b5397d55fdfcc5ae36b0919b
Frank Massi <frankmassi@gmail.com>
2013-05-17 21:27:28 -0500

    ruby 2 into 9 lines

--- Ruby/02.rb
+++ Ruby/02.rb
@@ -7,41 +7,12 @@
 # *
 # **************************************/
 
-class EvenFib
-	require 'pry'
-
-	attr_accessor :num_a, :num_b, :num_c, :sum
-
-	def initialize(num_a = 0, num_b = 1, sum = 0)
-		@num_a = num_a
-		@num_b = num_b
-		@num_c = @num_a + @num_b
-		@sum = sum
-	end
-
-	# binding.pry
-	
-	def next_num
-		@num_a = @num_b
-		@num_b = @num_c
-		@num_c = @num_a + @num_b
-	end
-
-	def is_even?(x)
-		true if x % 2 == 0
-	end
-
-	def sum
-		while @num_c < 4_000_000
-
-			if is_even?(@num_c) 
-				@sum += @num_c 
-			end
-			next_num
-		end
-		puts "#{@sum}"
-	end
+x = 0
+a = 1
+b = 2
+while b <= 4_000_000 
+	b % 2 == 0 ? x += b : nil
+	b = a + b
+	a = b - a
 end
-
-x = EvenFib.new
-x.sum
+puts x
