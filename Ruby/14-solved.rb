d99c1e9c1b974e7e7905c1253d3fa27d7e73e51e
Frank Massi <frankmassi@gmail.com>
2013-05-20 07:36:37 -0500

    ruby 14 solved

---
+++ Ruby/14-solved.rb
@@ -0,0 +1,35 @@
+# Longest Collatz sequence
+# Problem 14
+# The following iterative sequence is defined for the set of positive integers:
+
+# n  n/2 (n is even)
+# n  3n + 1 (n is odd)
+
+# Using the rule above and starting with 13, we generate the following sequence:
+
+# 13  40  20  10  5  16  8  4  2  1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+# Which starting number, under one million, produces the longest chain?
+
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+longest = 1
+answer = 1
+
+def collatz(n)
+	collatz_array = []
+	while n > 1
+		collatz_array << n
+		n % 2 == 0 ? (n = n/2) : (n = 3 * n + 1)
+	end
+	collatz_array << 1
+	return collatz_array
+end
+
+i = 2
+while i < 1_000_000
+	collatz(i).length > longest ? (answer = i ; longest = collatz(i).length) : nil
+	i += 1
+end
+
+puts "The answer is #{answer}"
