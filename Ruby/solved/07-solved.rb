f28adf47397da558a9cc66e2b71011017f386281
Frank Massi <frankmassi@gmail.com>
2013-06-15 11:41:14 -0500

    7 down 51x time by using mathn

--- Ruby/solved/07-solved.rb
+++ Ruby/solved/07-solved.rb
@@ -4,17 +4,19 @@
 # *																	*
 # ********************************************************************/
 timer_start = Time.now
-primes = [1,2,3]
-i = 5
+# primes = [1,2,3]
+# i = 5
 
-while primes.length <= 10_001
-	(1...primes.length).each {|p| 
-		(i % primes[p] != 0 && p == primes.length - 1) ? primes << i : nil
-		break if i % primes[p] == 0 
-	}
-	i += 2
-end
+# while primes.length <= 10_001
+# 	(1...primes.length).each {|p| 
+# 		(i % primes[p] != 0 && p == primes.length - 1) ? primes << i : nil
+# 		break if i % primes[p] == 0 
+# 	}
+# 	i += 2
+# end
 
-puts "The answer is #{primes.last}"
+require 'mathn'
+
+puts "The answer is #{Prime.first(10001).last}"
 
 puts "Elapsed Time: #{(Time.now - timer_start)*1000} milliseconds"
