9711f2211954254bf488ff888db7ed19169c6e5c
Frank Massi <frankmassi@gmail.com>
2013-06-17 08:06:08 -0500

    renamed files that need speeding

--- Ruby/solved/10-solved-SLOW.rb
+++ Ruby/solved/10-solved-SLOW.rb
@@ -9,7 +9,10 @@ require "mathn"
 
 answer = 0
 
-Prime.each {|p| answer += p; p >=2_000_000 ? (answer -= p; break) : nil }
+Prime.map do |p| 
+	break if p >= 2_000_000
+	answer += p 
+end
 
 puts "The answer is #{answer}"
 
